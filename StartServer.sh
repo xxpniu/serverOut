c7e842ddf978200dd7f424fcf293e2309aa017e4
xxp-macbook-pro <54249636@qq.com>
2016-08-12 00:45:03 +0800

    update

--- StartServer.sh
+++ StartServer.sh
@@ -1,7 +1,7 @@
 
-mono "./LoginServer/LoginServer.exe" "1900" "1800" "127.0.0.1" "Game_Account_DB" "root" "54249636">../LoginServer.txt&
+mono "./LoginServer/LoginServer.exe" "1900" "1800" "127.0.0.1" "game_account_db" "root" "54249636">../LoginServer.txt&
 echo "LoginServer started"
 sleep 1
-mono "./GateServer/GServer.exe" "1700" "2000" "127.0.0.1" "1800" "127.0.0.1" "Game_DB" "root" "54249636" "1" "../">../GServer.txt&
+mono "./GateServer/GServer.exe" "1700" "2000" "127.0.0.1" "1800" "127.0.0.1" "game_db" "root" "54249636" "1" "../">../GServer.txt&
 echo "GServer server 1 started"
 echo "Start server completed"
