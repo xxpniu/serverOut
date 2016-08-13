5c23818d542c875377924d9aa4677b12f51e927e
xxp-macbook-pro <54249636@qq.com>
2016-08-13 14:29:55 +0800

    add submit

--- StartServer.sh
+++ StartServer.sh
@@ -4,4 +4,7 @@ echo "LoginServer started"
 sleep 1
 mono "./GateServer/GServer.exe" "./server1.json">../GServer.txt&
 echo "GServer server 1 started"
+sleep 1 
+mono "./MapServer/MapServer.exe" "./mapserver.json">../MapServer.txt&
+echo "MapServer started"
 echo "Start server completed"
